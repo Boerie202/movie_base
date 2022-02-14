9d56f9917558d6d5fab9fa1d7f964e7a525bb316
Boerie202 <93001485+Boerie202@users.noreply.github.com>
2022-02-14 22:37:34 +0000

    auth.js file created for passport

---
+++ auth.js
@@ -0,0 +1,37 @@
+const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
+
+const jwt = require('jsonwebtoken'),
+  passport = require('passport');
+
+require('./passport'); // Local passport file
+
+
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username, // Username being encoded into the JWT
+    expiresIn: '7d', // This specifies that the token will expire in 7 days
+    algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
+  });
+}
+
+
+/* POST login code. */
+module.exports = (router) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user: user
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        return res.json({ user, token }); //ES6 shotrhand for: res.json({ user: user, token: token })
+      });
+    })(req, res);
+  });
+}
