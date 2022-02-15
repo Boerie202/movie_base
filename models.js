1c6f7d3cf20ea0ae6d9b27389a1462c7ec24d58d
Boerie202 <93001485+Boerie202@users.noreply.github.com>
2022-02-15 21:50:21 +0000

    Included password hashing

--- models.js
+++ models.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+//Secure login authentication code below
+const bcrypt = require('bcrypt');
+//----------------------------------------------
+
 let movieSchema = mongoose.Schema({
   Title: {type: String, required: true},
   Description: {type: String, required: true},
@@ -27,6 +31,19 @@ let userSchema = mongoose.Schema({
   Birthday: Date,
   FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
+
+
+//-------------Function to hash submitted passwords--------------------------------------------
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+//------------------------------------------------------------------------------------------------
+
+//-------------Function to compare submitted hash passwords to what's stored in the database-----
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
+//------------------------------------------------------------------------------------------------
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
