602b0119cd58724972600a6fcbdd9895196df449
Boerie202 <93001485+Boerie202@users.noreply.github.com>
2022-02-15 21:50:01 +0000

    Updated loads of code

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
-
-
+const { check, validationResult } = require('express-validator');//Server side validation tool
 
 
 
@@ -20,19 +19,40 @@ app.use(bodyParser.urlencoded({ extensed: true}));
 app.use(morgan('common'));
 app.use(bodyParser.json());
 
-//Code below to import the auth.js file
+//----------------------------------Code below to import the auth.js file-------------------------------------
+
+
+const cors = require('cors'); // Might need to move this up in code if nothing works
+
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 app.use(bodyParser.urlencoded({ extended: true}));
 let auth = require('./auth')(app);
 
-//Code below to require the passport module to import the passport.js file
+//-------------Code below to require the passport module to import the passport.js file-----------------------
+
+
 const passport = require('passport');
 require('./passport');
 
-//-------------------------------------Route calls below----------------------------
 
-//------------------------------------Create a new user----------------------------
-//CREATE
-//Add a user (JSON format)
+
+//--------------------------------------------------Route calls below-------------------------------------------
+
+//------------------------------------Create a new user/allow a new user to register----------------------------
+//CREATE CRUD Operation
+//Add a user (JSON format below)
 /*
 {
   ID: Integer,
@@ -41,21 +61,36 @@ require('./passport');
   Email: String,
   Birthday: Date
 }*/
-app.post('/users', passport.authenticate('jwt', { session: false}), (req, res) => {
+app.post('/users',
+
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail(), (req, res) => {
+
+
+    let errors = validationResult(req); // check the validation object for errors
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+ passport.authenticate('jwt', { session: false}), (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
-        return res.status(400).send(req.body.Username + 'User already exists');
+        return res.status(400).send(req.body.Username + ' already exists');
       } else {
         Users
           .create({
             Username: req.body.Username,
-            Password: req.body.Password,
+            Password: hashedPassword,
             Email: req.body.Email,
             Birthday: req.body.Birthday
           })
           .then((user) =>{res.status(201).json(user) })
-        .catch((error) => {
+          .catch((error) => {
           console.error(error);
           res.status(500).send('Error: ' + error);
         })
@@ -70,6 +105,8 @@ app.post('/users', passport.authenticate('jwt', { session: false}), (req, res) =
 
 //----------------------------------Read all user data---------------------------------
 
+
+//READ CRUD Operation
 app.get('/users', passport.authenticate('jwt', { session: false}), (req, res) => {
   Users.find()
   .then((users) => {
@@ -84,6 +121,8 @@ app.get('/users', passport.authenticate('jwt', { session: false}), (req, res) =>
 
 //-----------------------------Get a user by username---------------------------------
 
+
+//READ CRUD Operation
 app.get('/users/:Username', passport.authenticate('jwt', { session: false}), (req, res) => {
   Users.findOne({ Username: req.params.Username })
     .then((user) => {
@@ -94,6 +133,8 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false}), (re
       res.status(500).send('Error: ' + err);
     });
 });
+
+
 
 //--------------------------Update a user's info, by username----------------------------
 /* Use JSON in this format
@@ -130,6 +171,8 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false}), (re
 
 //--------------------------------Add a movie to a user's list of favorites---------------------------------
 
+
+//CREATE CRUD Operation
 app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false}), (req, res) => {
   Users.findOneAndUpdate({ Username: req.params.Username }, {
      $push: { FavoriteMovies: req.params.MovieID }
@@ -147,6 +190,8 @@ app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { sess
 
 
 //-----------------------------------Delete a user by username-------------------------------
+
+//DELETE CRUD Operation
 app.delete('/users/:Username', passport.authenticate('jwt', { session: false}), (req, res) => {
   Users.findOneAndRemove({ Username: req.params.Username })
     .then((user) => {
@@ -163,8 +208,11 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false}), 
 });
 
 
+
 //---------------------------Allow users to remove a movie from their list of favourites-----------------
 
+
+//DELETE CRUD Operation
 app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false}), (req, res) => {
   Users.findOneAndUpdate({ Username: req.params.Username }, {
      $pull: { FavoriteMovies: req.params.MovieID }
@@ -181,16 +229,19 @@ app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { se
 });
 
 
+
 //-----------------------------------------------------------------------------------------------------
 
-//READ
+
+//READ CRUD Operation
 app.get('/',(req, res) => {
   res.send('Welcome to Movie Base!');
 });
 
-//-----------------------------Returns a list of all the movies-----------------------------
-//READ
 
+//-----------------------------Returns a list of all the movies-----------------------------
+
+//READ CRUD Operation
 app.get('/movies', passport.authenticate('jwt', { session: false}), (req, res) => {
   Movies.find()
   .then((movies) => {
@@ -202,14 +253,13 @@ app.get('/movies', passport.authenticate('jwt', { session: false}), (req, res) =
   });
 });
 
-/*app.get('/movies', (req, res) => {
-  res.status(200).json(Movies);
-});
 
-app.use(express.static("public"));*/
+
 
 //------------------------Returns data for a movie by using the title------------------------
-//READ
+
+
+//READ CRUD Operation
 app.get('/movies/:title', passport.authenticate('jwt', { session: false}), (req, res) => {
 const { title } = req.params;
 const movie = Movies.find (movie => movie.Title === title);
@@ -219,11 +269,14 @@ if (movie) {
 } else {
   res.status(400).send('No movie by this name')
 }
-
 });
 
+
+
 //-------------------------Return data about a genre by using the genre name-----------------
-//READ
+
+
+//READ CRUD Operation
 app.get('/movies/genre/:genreName', passport.authenticate('jwt', { session: false}), (req, res) => {
 const {genreName} = req.params;
 const genre = Movies.find (movie => movie.Genre.Name === genreName).Genre;
@@ -233,11 +286,13 @@ if (genre) {
 } else {
   res.status(400).send('No genre by this name')
 }
-
 });
 
+
 //----------------------------Return data about a drector by name------------------------
-//READ
+
+
+//READ CRUD Operation
 app.get('/movies/director/:directorName', passport.authenticate('jwt', { session: false}), (req, res) => {
 const {directorName} = req.params;
 const director = MOvies.find (movie => movie.Director.Name === directorName).Director;
@@ -247,20 +302,23 @@ if (director) {
 } else {
   res.status(400).send('No director by this name')
 }
-
 });
 
 
 
 //-----------------------------Error handler-----------------------------
 
+
 app.use((err, req, res, next) =>{
   console.error(err.stack);
   res.status(500).send("Something broke, sorry! We'll get right on that")
 });
 
+
 //------------------------Listener for port 8080-------------------------
+
+
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Movie Base is listening on port ${port}`);
 });
