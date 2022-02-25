ebb4278537ae0c742cecc89b06512f5d094f0e96
Boerie202 <93001485+Boerie202@users.noreply.github.com>
2022-02-25 15:37:57 +0000

    Changed code back

--- index.js
+++ index.js
@@ -323,10 +323,5 @@ app.use((err, req, res, next) =>{
 
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Movie Base is listening on` + port );
+  console.log(`Movie Base is listening on port ${port}`);
 });
-
-// const port = process.env.PORT || 8080;
-// app.listen(port, '0.0.0.0', () => {
-//   console.log(`Movie Base is listening on port ${port}`);
-// });
