2598b2efdc8b0613e4b6b2b371c9da6efb12326e
Boerie202 <93001485+Boerie202@users.noreply.github.com>
2022-02-28 11:16:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -64,48 +64,6 @@ require('./passport');
   Email: String,
   Birthday: Date
 }*/
-// app.post('/users',
-//     [
-//     check('Username', 'Username is required').isLength({min: 5}),
-//     check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-//     check('Password', 'Password is required').not().isEmpty(),
-//     check('Email', 'Email does not appear to be valid').isEmail()],
-//
-//   (req, res) => {
-//
-//
-//     let errors = validationResult(req); // check the validation object for errors
-//
-//     if (!errors.isEmpty()) {
-//       return res.status(422).json({ errors: errors.array() });
-//     }
-//
-//  passport.authenticate('jwt', { session: false}), (req, res) => {
-//   let hashedPassword = Users.hashPassword(req.body.Password);
-//   Users.findOne({ Username: req.body.Username })
-//     .then((user) => {
-//       if (user) {
-//         return res.status(400).send(req.body.Username + ' already exists');
-//       } else {
-//         Users
-//           .create({
-//             Username: req.body.Username,
-//             Password: hashedPassword,
-//             Email: req.body.Email,
-//             Birthday: req.body.Birthday
-//           })
-//           .then((user) =>{res.status(201).json(user) })
-//           .catch((error) => {
-//           console.error(error);
-//           res.status(500).send('Error: ' + error);
-//         })
-//       }
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//       res.status(500).send('Error: ' + error);
-//     });
-// });
 
 
 app.post('/users',
